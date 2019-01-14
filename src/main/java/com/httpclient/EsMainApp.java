e754f03e1a3360c8b5fec665fd79261aaf1da45d
tangjie <tangjie03@baidu.com>
2019-01-14 14:27:20 +0800

    test

---
+++ src/main/java/com/httpclient/EsMainApp.java
@@ -0,0 +1,18 @@
+package com.httpclient;
+
+import org.junit.Test;
+
+/**
+ * @author: tangjie03@baidu.com
+ * @Date: 2018-12-12 18:04
+ * @Description:
+ */
+public class EsMainApp {
+
+    private static String esIp = "0.0.0.0";
+
+    @Test
+    public void testPut(){
+
+    }
+}
